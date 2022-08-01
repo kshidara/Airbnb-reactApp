78f85fe2746ec375fdc3e3573f13c3e0028e6bbe
Kazune Shidara <kazuneshidara@gmail.com>
2022-07-31 22:58:21 -0700

    Start props practice

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -1,16 +1,25 @@
 import React from "react";
 
+export default function Contact({img, name, email, phone}) {
+    return (
+        <div className="contact-card">
+            <img src={img}/>
+            <h3>{name}</h3>
+            <div className="info-group">
+                <img src="./images/phone-icon.png" />
+                <p>{phone}</p>
+            </div>
+            <div className="info-group">
+                <img src="./images/mail-icon.png" />
+                <p>{email}</p>
+            </div>
+        </div>
+    )
+}
 
-<div className="contact-card">
-        <img src="./images/mr-whiskerson.png"/>
-        <h3>Mr. Whiskerson</h3>
-        <div className="info-group">
-            <img src="./images/phone-icon.png" />
-            <p>(212) 555-1234</p>
-        </div>
-        <div className="info-group">
-            <img src="./images/mail-icon.png" />
-            <p>mr.whiskaz@catnap.meow</p>
-        </div>
-</div>
-            
+const person = {
+    img: "./images/mr-whiskerson.png",
+    name: "Mr. Whiskerson",
+    phone: "(800) 555-1234",
+    email: "mr.whiskaz@catnap.meow"
+}

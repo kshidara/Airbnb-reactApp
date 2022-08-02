9525df16389713de469f03744d9416706696a9e6
Kazune Shidara <kazuneshidara@gmail.com>
2022-08-01 21:55:22 -0700

    passing in non-string datatypes as props into components

--- my-app/src/Contact.js
+++ my-app/src/Contact.js
@@ -1,25 +1,12 @@
-/* Challenge:
-One LAST time in this course, set up a React app from scratch
-- Render an <App /> component
-    - App should be in its own file
-- App should render 4-5 <Joke /> components 
-  (Joke component defined in its own file too)
-    - Each Joke should receive a "setup" prop and a "punchline" prop
-      and render those however you'd like
-- Use your favorite 2-part jokes (setup & punchline), or check
-  jokes.md file for some examples.
+/*
+Challenge: Think critically - how would you pass in a prop that wasn't
+a string datatype.   --> make use of the curly brakets!!!
 
-
-
-EXTRA CREDIT:
-Some jokes are only a punchline with no setup:
-
-E.g.: "It’s hard to explain puns to kleptomaniacs because 
-they always take things literally."
-
-If you don't pass in a "question" prop, how might you make it only 
-show the punchline?
+E.g. Say you want each Joke component to receive an "upvotes" and "downvotes"
+prop that is a number, as well as a prop with an array of comments, and a boolean
+of whether the joke is a pun (`isPun`).
 */
+
 
 import React from "react"
 import Joke from "./Joke"
@@ -31,22 +18,33 @@ export default function App() {
             <Joke 
                 setup="I got my daughter a fridge for her birthday." 
                 punchline="I can't wait to see her face light up when she opens it." 
+                isPun={true}
+                upvotes={10}
+                downvotes={2}
+
             />
             <Joke 
                 setup="How did the hacker escape the police?" 
                 punchline="He just ransomware!" 
+                isPun={true}
+
             />
             <Joke 
                 setup="Why don't pirates travel on mountain roads?" 
                 punchline="Scurvy." 
+                isPun={true}
             />
             <Joke 
                 setup="Why do bees stay in the hive in the winter?" 
                 punchline="Swarm." 
+                isPun={true}
+
             />
             <Joke 
                 setup="What's the best thing about Switzerland?" 
                 punchline="I don't know, but the flag is a big plus!" 
+                isPun={true}
+
             />
 
 

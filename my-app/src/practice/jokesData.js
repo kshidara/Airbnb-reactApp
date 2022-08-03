3f2ae2ed82d7fbd2ea52fd48965de42f3e651776
Kazune Shidara <kazuneshidara@gmail.com>
2022-08-02 22:45:56 -0700

    updating AppJokes to use map() to render each prop in jokesData file and return jokesElement to have all jokes appear on page

--- my-app/src/practice/jokesData.js
+++ my-app/src/practice/jokesData.js
@@ -2,23 +2,23 @@
 
 export default [
     {
-        setup: "I got my daughter a fridge for her birthday.",
+        question: "I got my daughter a fridge for her birthday.",
         punchline: "I can't wait to see her face light up when she opens it."
     },
     {
-        setup: "How did the hacker escape the police?",
+        question: "How did the hacker escape the police?",
         punchline: "He just ransomware!"
     },
     {
-        setup: "Why don't pirates travel on mountain roads?",
+        question: "Why don't pirates travel on mountain roads?",
         punchline: "Scurvy."
     },
     {
-        setup: "Why do bees stay in the hive in the winter?",
+        question: "Why do bees stay in the hive in the winter?",
         punchline: "Swarm."
     },
     {
-        setup: "What's the best thing about Switzerland?",
+        question: "What's the best thing about Switzerland?",
         punchline: "I don't know, but the flag is a big plus!"
     }
 ]

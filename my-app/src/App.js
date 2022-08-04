0ce1888a0db9f6225330afa6425b2e98526bb010
Kazune Shidara <kazuneshidara@gmail.com>
2022-08-04 00:31:15 -0700

    updated how to pass in props to Card component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -5,23 +5,25 @@ import Card from "./Card"
 import data from "./data"
 
 
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Spread_syntax#spread_in_object_literals
+
 export default function App() {
 
   const cards = data.map(eachData => {
     return <Card  
               key={eachData.id}
-              img={eachData.coverImg}
-              rating={eachData.stats.rating} 
-              reviewCount={eachData.stats.reviewCount}
-              location={eachData.location}
-              title={eachData.title}
-              price={eachData.price}
+              // item={eachData}
+              {...eachData}
+              // above format spreads all properties as separate props into Card component
+              // going back to id={item.id} title={item.title}  description={item.description} 
+
             />
   })  
 
   return (
     <div>
       <Navbar />
+      <Hero  />
       <section className="cards-list">
         {cards}
       </section>    
--- my-app/src/Card.js
+++ my-app/src/Card.js
@@ -4,13 +4,23 @@ import starPhoto from './images/star.png';
 
 export default function Card(props) {
 
+    let badgeText
+    if (props.openSpots === 0) {
+        badgeText = "SOLD OUT"
+    } else if (props.location === "Online") {
+        badgeText = "ONLINE"
+    }
+
+
+
     return (
         <div className="card">
-            <img className="card--photo" src={props.img} ></img>
+            {badgeText && <div className="card--badge">{badgeText}</div>}
+            <img className="card--photo" src={props.coverImg} ></img>
             <div className="card--stats">
                 <img className="star--photo" src={starPhoto} ></img>
-                <span className="gray">{props.rating}</span>
-                <span className="gray">({props.reviewCount}) * {props.country}</span>
+                <span className="gray">{props.stats.rating}</span>
+                <span className="gray">({props.stats.reviewCount}) * {props.country}</span>
             </div> 
             <p className="title">{props.title}</p>
             <p className="cost--person"><span className="bold">From ${props.price} </span> / person</p>
